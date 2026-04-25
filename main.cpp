e9e5a35ddb4170f2349fa4d70d800107d813a233
RobinSong <songqizhen006@gmail.com>
2026-04-25 15:10:46 +0100

    Add shading

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ const TGAColor red = {{0, 0, 255, 255}, 4};
 const TGAColor blue = {{255, 255, 0, 255}, 4};
 const TGAColor green = {{0, 255, 0}, 4};
 
-const int height = 400;
-const int width = 400;
+const int height = 1024;
+const int width = 1024;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 {
@@ -57,7 +57,7 @@ vec3 barycentric(vec2 A, vec2 B, vec2 C, vec2 P)
     float v = w.y / w.z;
     return vec3{1.f - (u + v), u, v};
 }
-void triangle(vec3 *pts, TGAImage &image, TGAColor color, std::vector<double> &zbuffer)
+void triangle(vec3 *pts, vec2 *uvs, TGAImage &image, TGAImage &diffuse_map, std::vector<double> &zbuffer, double *intensity)
 {
     int minX = std::max(0, (int)std::floor(std::min(pts[0].x, std::min(pts[1].x, pts[2].x))));
     int maxX = std::min(image.width() - 1, (int)std::ceil(std::max(pts[0].x, std::max(pts[1].x, pts[2].x))));
@@ -77,6 +77,20 @@ void triangle(vec3 *pts, TGAImage &image, TGAColor color, std::vector<double> &z
                 frag_z += pts[i].z * ba_screen[i];
             }
 
+            double u_p = ba_screen.x * uvs[0].x + ba_screen.y * uvs[1].x + ba_screen.z * uvs[2].x;
+            double v_p = ba_screen.x * uvs[0].y + ba_screen.y * uvs[1].y + ba_screen.z * uvs[2].y;
+
+            int tex_idx = u_p * diffuse_map.width();
+            int tex_idy = v_p * diffuse_map.height();
+
+            TGAColor color = diffuse_map.get(tex_idx, tex_idy);
+
+            // double b_intensity = ba_screen.x * intensity[0] + ba_screen.y * intensity[1] + ba_screen.z * intensity[2];
+
+            // for (int i = 0; i < 3; i++) {
+            //     color.bgra[i] *= b_intensity; 
+            // }
+
             int idx = x + y * width;
             if (zbuffer[idx] < frag_z) {
                 zbuffer[idx] = frag_z;
@@ -86,46 +100,61 @@ void triangle(vec3 *pts, TGAImage &image, TGAColor color, std::vector<double> &z
     }
 }
 
-void gradienttriangle(vec3 *pts, TGAImage &image, TGAColor *colors)
+// void gradienttriangle(vec3 *pts, TGAImage &image, TGAColor *colors)
+// {
+//     vec2 bboxmin = {pts[0].x, pts[0].y};
+//     vec2 bboxmax = {pts[0].x, pts[0].y};
+//     vec2 clamp = {(double)image.width() - 1, (double)image.height() - 1}
+//     // find bbox
+//     for (int i = 0; i < 3; i++)
+//     {
+//         for (int j = 0; j < 2; j++)
+//         {
+//             bboxmin[j] = std::max(0.0, std::min(bboxmin[j], pts[i][j]));
+//             bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
+//         }
+//     }
+//     vec2 P;
+//     for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++)
+//     {
+//         for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++)
+//         {
+//             vec3 ba_screen = barycentric(pts[0].xy(), pts[1].xy(), pts[2].xy(), P);
+//             if (ba_screen.x < 0 || ba_screen.y < 0 || ba_screen.z < 0)
+//                 continue;
+//             unsigned char r = ba_screen.x * colors[0].bgra[0] + ba_screen.y * colors[1].bgra[0] + ba_screen.z * colors[2].bgra[0];
+//             unsigned char g = ba_screen.x * colors[0].bgra[1] + ba_screen.y * colors[1].bgra[1] + ba_screen.z * colors[2].bgra[1];
+//             unsigned char b = ba_screen.x * colors[0].bgra[2] + ba_screen.y * colors[1].bgra[2] + ba_screen.z * colors[2].bgra[2];
+//             image.set(P.x, P.y, TGAColor{r, g, b, 255});
+//         }
+//     }
+// }
+
+
+int main()
 {
-    vec2 bboxmin = {pts[0].x, pts[0].y};
-    vec2 bboxmax = {pts[0].x, pts[0].y};
-    vec2 clamp = {(double)image.width() - 1, (double)image.height() - 1};
+    //construct image
+    TGAImage image(width, height, TGAImage::RGB);
 
-    // find bbox
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            bboxmin[j] = std::max(0.0, std::min(bboxmin[j], pts[i][j]));
-            bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
-        }
-    }
+    //load model
+    Model model ("./obj/african_head/african_head.obj");
 
-    vec2 P;
-    for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++)
-    {
-        for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++)
-        {
-            vec3 ba_screen = barycentric(pts[0].xy(), pts[1].xy(), pts[2].xy(), P);
-            if (ba_screen.x < 0 || ba_screen.y < 0 || ba_screen.z < 0)
-                continue;
+    //init zbuffer
+    std::vector<double> zbuffer(width * height, -std::numeric_limits<double>::max());
 
-            unsigned char r = ba_screen.x * colors[0].bgra[0] + ba_screen.y * colors[1].bgra[0] + ba_screen.z * colors[2].bgra[0];
-            unsigned char g = ba_screen.x * colors[0].bgra[1] + ba_screen.y * colors[1].bgra[1] + ba_screen.z * colors[2].bgra[1];
-            unsigned char b = ba_screen.x * colors[0].bgra[2] + ba_screen.y * colors[1].bgra[2] + ba_screen.z * colors[2].bgra[2];
+    //init light direction
+    vec3 lightdir{-1,0,-1};
 
-            image.set(P.x, P.y, TGAColor{r, g, b, 255});
-        }
-    }
-}
+    //load diffuse
+    TGAImage diffuse_map;
+    diffuse_map.read_tga_file("./obj/african_head/african_head_diffuse.tga");
 
-void rendermodel(vec3 lightdir, TGAImage &image, Model &model, std::vector<double> &zbuffer)
-{
+    //render model
     for (int i = 0; i < model.nfaces(); i++)
     {
         vec3 worldcoor[3];
         vec3 screencoor[3];
+        vec2 uvs[3];
 
         for (int j = 0; j < 3; j++)
         {
@@ -136,60 +165,27 @@ void rendermodel(vec3 lightdir, TGAImage &image, Model &model, std::vector<doubl
                 (vertex.y + 1.) * image.height() / 2., 
                 (vertex.z + 1.) * 255. / 2.
             };
+            uvs[j] = model.uv(i,j);
         }
 
         //normal
         vec3 normal = cross((worldcoor[2] - worldcoor[0]), (worldcoor[1] - worldcoor[0]));
         normal = normalized(normal);
 
-        //lighting intensity
-        double intensity = normal * lightdir; 
+        //obj nrm intensity
+        double intensity[3];
+        for (int i = 0; i < 3; i++)
+        {
+            intensity[i] = model.normal(uvs[i]).xyz() * lightdir;
+        }
 
-        if (intensity > 0) {
-        // 根据强度着色 (Flat Shading)
-        triangle(screencoor, image, TGAColor{
-            (unsigned char)(intensity * 255), 
-            (unsigned char)(intensity * 255), 
-            (unsigned char)(intensity * 255), 
-            255
-        }, zbuffer);
+        // Flat Shading
+        triangle(screencoor, uvs, image, diffuse_map, zbuffer, intensity);
     }
-    }
-}
-
-int main()
-{
-    TGAImage image(width, height, TGAImage::RGB);
-
-    Model model ("./obj/african_head/african_head.obj");
-
-    std::vector<double> zbuffer(width * height, -std::numeric_limits<double>::max());
-
-    rendermodel(vec3{0,0,-1}, image, model, zbuffer);
-
-    // image.flip_vertically();
 
     TGAImage ZbufferImg(width, height, TGAImage::RGB);
 
-    for (int i = 0; i < width; i++)
-    {
-        for (int j = 0; j < height; j++)
-        {
-            double z_val = zbuffer[i + j * width]; // 先拿 double 值
-
-            // 1. 必须在转 unsigned char 之前做判断！
-            if (z_val < -1000) {
-                ZbufferImg.set(i, j, TGAColor{0, 0, 0, 255});
-                continue;
-            }
-            
-            unsigned char z = (unsigned char)std::min(255.0, std::max(0.0, z_val));
-            ZbufferImg.set(i, j, TGAColor{z, z, z, 255});
-        }
-        
-    }
-
-    image.write_tga_file("./result/rendermodel1.tga");
+    image.write_tga_file("./result/rendermodel3.tga");
     ZbufferImg.write_tga_file("./result/zbuffer.tga");
 
     return 0;
